5d193845980be39341072b0fced744b1e9deabe6
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-07-04 18:40:15 +0300

    requirements.txt and packages

--- flask/flask_auth_app/auth/__init__.py
+++ flask/flask_auth_app/auth/__init__.py
@@ -1,1 +1,2 @@
 from .basic.basic_auth import basic_auth
+from .jwt_token.token_auth import token_auth, generate_token
--- flask/flask_auth_app/resources/__init__.py
+++ flask/flask_auth_app/resources/__init__.py
@@ -1,1 +1,2 @@
 from .login import Login
+from .user import Users, LoginToken, RefreshToken
