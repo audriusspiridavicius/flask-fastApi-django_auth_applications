89e4c6e62882d95894b7dc80d81d841907139b02
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-07-04 18:39:15 +0300

    create_app database name parameter added

--- flask/flask_auth_app/__init__.py
+++ flask/flask_auth_app/__init__.py
@@ -11,14 +11,15 @@ def init_auth_api(app):
     auth_api = Api(app)
     
     auth_api.add_resource(Login, '/login','/')
+    auth_api.add_resource(Users, '/users')
     auth_api.add_resource(LoginToken, '/login-token')
     auth_api.add_resource(RefreshToken, '/refresh-token')
     
-def create_app()->Flask:
+def create_app(db_name = "sqlite:///users.db")->Flask:
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
-    
-    db.init_app(app)
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_name
+    with app.app_context():
+        db.init_app(app)
     create_database(app)
     init_auth_api(app)
     fill_database_test_data(app)
