8a714d1bbd991bb0d93bc5b87d00f46b28d50afc
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-07-18 15:15:46 +0300

    user login/verify password methods

--- fastApi/authApp/database/user.py
+++ fastApi/authApp/database/user.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import mapped_column, Mapped, Session
 from fastapi import Depends
 from typing import Annotated
 from sqlalchemy.ext.hybrid import hybrid_property
-from passlib.hash import sha512_crypt
+
 
 class User(Base):
     __tablename__ = "users"
@@ -13,4 +13,29 @@ class User(Base):
     _password:Mapped[str] = mapped_column("password", nullable=False)
     
    
+    
+    @classmethod
+    def login(cls, username:str, password:str, db:Session):
+        
+        user:User = db.query(User).filter_by(username=username).first()
+        
+        if user:
+            if cls.verify_password(user.password, password):
+                return user
+        return None
+    
+    
+    @classmethod
+    def verify_password(cls, actual_password, entered_password):
+        return actual_password == entered_password
+    
+    @hybrid_property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = password
+        
+        
     
