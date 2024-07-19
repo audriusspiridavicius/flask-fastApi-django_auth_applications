33f6485e678b029492c5c63cbb3ce7e31affce49
audrius spiridavicius <audriusspiridavicius@gmail.com>
2024-07-19 12:15:27 +0300

    test database details

--- fastApi/authApp/tests/db.py
+++ fastApi/authApp/tests/db.py
@@ -5,9 +5,6 @@ from sqlalchemy.pool import StaticPool
 
 from sqlalchemy.orm import declarative_base
 
-from authApp.database.database import get_database
-from authApp.app import app
-
 SQLALCHEMY_DATABASE_URL_TEST = "sqlite:///test_db.db"
 
 test_engine = create_engine(
@@ -22,17 +19,8 @@ Base = declarative_base()
 
 
 def get_test_database():
-    from authApp.database.user import User 
-    Base.metadata.create_all(bind=test_engine)
     db = TestingSessionLocal()
     try:
         yield db
     finally:
         db.close()
-
-
-
-app.dependency_overrides[get_database] = get_test_database
-
-# test_client = TestClient(app)
-
