d3d9fa5d1cf1730f88f9f176e8b3071770d2939f
audrius spiridavicius <audriusspiridavicius@gmail.com>
2024-07-19 12:16:45 +0300

    user model tests

--- fastApi/authApp/tests/test_admin_user_creation.py
+++ fastApi/authApp/tests/test_admin_user_creation.py
@@ -1,24 +1,41 @@
-from fastapi.testclient import TestClient
 
-from authApp.app import app
-from .db import get_test_database, test_engine, Base, TestingSessionLocal
+from .db import test_engine, TestingSessionLocal
 import pytest
 from sqlalchemy.orm import Session
+from authApp.database.user import User, Base
 
+@pytest.fixture(scope="session", autouse=True)
+def db():
 
-# @pytest.fixture(autouse=True)
-# def setup_database():
+    db = TestingSessionLocal()
+    Base.metadata.create_all(bind=test_engine)
+    try:
+        yield db
+    finally:
+        db.close()
+        Base.metadata.drop_all(bind=test_engine)
 
-#     Base.metadata.create_all(bind=test_engine)
-#     # Base.metadata.drop_all(bind=test_engine)
+@pytest.fixture
+def admin_user(db:Session):
+    User.create_init_users(db)
+    admin = db.query(User).filter_by(username="admin").first()
+    return admin
 
-def test_admin_user_created():
-    db:Session = get_test_database()
-# We grab another session to check 
-    # if the items are created
-    client = TestClient(app)
-    # users = db.query(User).all()
+def test_admin_user_created(db:Session):
+
+    User.create_init_users(db)
+    users = db.query(User).count()
     
-    assert 1 == 1
+    assert users == 1
 
+@pytest.fixture
+def get_password():
+    return "admin"
 
+def test_admin_password_hashed(admin_user:User, get_password):
+ 
+    assert admin_user.verify_password(admin_user.password, get_password) == True
+    
+def test_admin_password_not_equal_entered_value(admin_user:User, get_password):
+    
+    assert admin_user.password != get_password
