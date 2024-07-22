db45b4e62dc3ad7afdb6c673d8d630da32342cef
audrius spiridavicius <audriusspiridavicius@gmail.com>
2024-07-22 09:46:14 +0300

    create admin user. create_init_users called

--- fastApi/authApp/database/database.py
+++ fastApi/authApp/database/database.py
@@ -16,6 +16,7 @@ def get_database():
     Base.metadata.create_all(bind=engine)
     try:
         db = SessionLocal()
+        User.create_init_users(db)
         yield db
     finally:
         db.close()
